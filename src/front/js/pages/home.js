bca3208298bd52f498d2bbf563143846da153713
Roberto Torres Martínez <roberxiri@gmail.com>
2025-03-05 11:20:17 +0000

    Vista Individual Phone

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,12 +2,14 @@ import "../../styles/home.css";
 import React from "react";
 import {Welcome} from '../component/welcome'
 import {Onfire} from '../component/onfire'
+import { LogIn } from "./logIn";
+import { SignUp } from "./signUp";
 
 export const Home = () => {
 
 	return (
 		<>
-		<div style={{backgroundColor: "rgb(47, 65, 79)"}}>
+		<div className="page-content" style={{backgroundColor: "rgb(47, 65, 79)"}}>
 		{/* <Welcome/> */}
 		<Onfire/>
 		</div>
