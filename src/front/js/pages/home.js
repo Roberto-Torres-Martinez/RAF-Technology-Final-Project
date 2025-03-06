7b68f50549db0ad5645d7564631613beb09985e4
Roberto Torres Martínez <roberxiri@gmail.com>
2025-03-06 10:08:15 +0000

    Styles in line Log In and Sign Up

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,14 +4,15 @@ import {Welcome} from '../component/welcome'
 import {Onfire} from '../component/onfire'
 import { LogIn } from "./logIn";
 import { SignUp } from "./signUp";
+import { VistaIndividualPhone } from "./vistaIndividualPhone";
 
 export const Home = () => {
 
 	return (
 		<>
 		<div className="page-content" style={{backgroundColor: "rgb(47, 65, 79)"}}>
-		{/* <Welcome/> */}
-		<Onfire/>
+		<Welcome/>
+		{/*<Onfire/>*/}
 		</div>
 		</>
 	);
