e12d6ffe428e9c20640cabf00f3d26ca64437b17
Roberto Torres Martínez <roberxiri@gmail.com>
2025-03-07 20:50:07 +0000

    titulo productos relacionados centrado y cambiado de color, mb añadido

--- src/front/js/component/related-products.js
+++ src/front/js/component/related-products.js
@@ -5,22 +5,22 @@ export const RelatedProducts = () => {
     return (
         <>
             <div className="container my-5 related-container">
-                <h1 className="ms-5 mb-2">Productos Relacionados (Smartphones)</h1>
+                <h1 className="mb-5 text-white text-center">Productos Relacionados (Smartphones)</h1>
                 <div className="section container mx-3 pt-5 px-5 pb-1 border-techno border-4 mt-4 mx-auto">
                     <div className="row justify-content-around">
-                        <div className="col-md-3">
+                        <div className="col-md-3 mb-5">
                             <ProductCardSmall />
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 mb-5">
                             <ProductCardSmall />
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 mb-5">
                             <ProductCardSmall />
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 mb-5">
                             <ProductCardSmall />
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 mb-5">
                             <ProductCardSmall />
                         </div>
                     </div>
