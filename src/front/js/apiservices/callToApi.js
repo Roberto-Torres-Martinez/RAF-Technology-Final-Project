83942e469130c1f69e329ab12862bf8fdd3d9a98
Facundo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-03-07 12:41:52 +0000

    Agregado el swal y responde al token del usuario

--- src/front/js/apiservices/callToApi.js
+++ src/front/js/apiservices/callToApi.js
@@ -1,5 +1,5 @@
 const urlBackend = process.env.BACKEND_URL
-
+import Swal from "sweetalert2";
 export const createUser = async (newUser) =>{
     try {
         const response = await fetch(urlBackend + 'user-signup', {
@@ -48,7 +48,19 @@ export const login = async (user, responseApi, navigate) =>{
     sessionStorage.setItem('token', data.token) 
     if(data.token){
         navigate('/')
-        window.location.reload();
+        Swal.fire({
+                title: "Bienvenido!",
+                text: "A partir de ahora estas dentro del equipo rocket",
+                icon: "success",
+                imageUrl: "https://media0.giphy.com/media/uzWoRrlxnbL6TJgIbP/giphy.gif?cid=6c09b952p0cq28z0rgmkvl41gkn11yj4231dug2es15s9g0r&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+                draggable: true,
+                confirmButtonText:`<i class="fa fa-thumbs-up"></i> Great!`
+                 }).then((response)=>{
+                    if(response.isConfirmed){
+                        window.location.reload()
+                    }
+                 });
+
     };
 };
 
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,5 +1,5 @@
 import "../../styles/home.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {Welcome} from '../component/welcome'
 import {Onfire} from '../component/onfire'
 
@@ -15,32 +15,31 @@ import { privateUser } from "../apiservices/callToApi";
 
 export const Home = () => {
 
-	const{store, actions} = useContext(Context)
+const [response, setResponse] = useState(null)
 
+const checkoutUser = async () => {
+	const verified = await privateUser()
+	setResponse(verified)
+}
 
-	useEffect(()=>{
-		if(actions){
-			Swal.fire({
-				title: "Bienvenido!",
-				text: "A partir de ahora estas dentro del equipo rocket",
-				icon: "success",
-				imageUrl: "https://media0.giphy.com/media/uzWoRrlxnbL6TJgIbP/giphy.gif?cid=6c09b952p0cq28z0rgmkvl41gkn11yj4231dug2es15s9g0r&ep=v1_gifs_search&rid=giphy.gif&ct=g",
-				draggable: true
-			  });
+	
+if(response){
+	Swal.fire({
+		title: "Bienvenido!",
+		text: "A partir de ahora estas dentro del equipo rocket",
+		icon: "success",
+		imageUrl: "https://media0.giphy.com/media/uzWoRrlxnbL6TJgIbP/giphy.gif?cid=6c09b952p0cq28z0rgmkvl41gkn11yj4231dug2es15s9g0r&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+		draggable: true
+		 });
 		}
-	})
-
-	useEffect(()=>{
-		privateUser();
-	},[])
+	
 
 	return (
 		<>
 
 		<div  className="page-content" style={{backgroundColor: "rgb(47, 65, 79)"}}>
 		<Welcome/>
-		<div className="container mx-auto py-4">
-			<button onClick={actions.setModal}>Modal</button>	
+		<div className="container mx-auto py-4">	
 		<Onfire/>
 		<HomeCatalog/>
 		<NovedadesMail/>
