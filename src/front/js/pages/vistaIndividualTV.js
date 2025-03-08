df09438ad21e64dca16d412a928b2ec467a675b9
Roberto Torres Martínez <roberxiri@gmail.com>
2025-03-08 12:23:35 +0000

    Vistas individuales básicas de producto hechas

--- src/front/js/pages/vistaIndividualTV.js
+++ src/front/js/pages/vistaIndividualTV.js
@@ -1,7 +1,157 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { RelatedProducts } from "../component/related-products";
 
 export const VistaIndividualTv = () => {
+
+    const handleImageColors = (imageUrl) => {
+        setImageColors(imageUrl);
+    };
+
     return (
-        <h1>Vista Individual TV</h1>
-    )
-}
+        <div className="container">
+            <div className="row col-md-12">
+                <div className="row col-md-6 card-individual-tv-image">
+                    <img className="" src="https://www.bhphotovideo.com/images/images2500x2500/samsung_qn65qn90cafxza_neo_qled_qn90c_65_1742740.jpg" alt="Product" />
+                </div>
+                <div className="col-md-6 text-white">
+                    <div className="card-body-individual">
+                        <h5 className="card-title sub-titulo mb-3">QN90C Neo QLED</h5>
+                        <p className="card-text-score"></p>
+                        <p className="card-text-price sub-titulo">
+                            <small className="text-white">1.499,00 €</small>
+                        </p>
+                        <p className="card-text">
+                            <button className="btn-add-cart">
+                                <i className="fa-solid fa-cart-plus"></i> Añadir al carrito
+                            </button>
+                        </p>
+                        <div className="accordion" id="accordionExample">
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTop" aria-expanded="false" aria-controls="collapseTop">
+                                        <strong>Descripción</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseTop" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>Televisor 4K con tecnología Neo QLED para un contraste mejorado y colores más vivos.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        <strong>Marca</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>Samsung</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                        <strong>Pantalla</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>55 pulgadas</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        <strong>Medidas</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>1230 x 710 x 28 mm</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                        <strong>Usos Recomendados</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>Visualización de contenido en 4K, juegos, cine en casa</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                        <strong>Año Modelo</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseSix" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>2024</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                        <strong>Conectividad</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseSeven" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>HDMI 2.1, Wi-Fi, Bluetooth, USB</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                                        <strong>Contenido De La Caja</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseEight" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>Televisor, mando a distancia, cable de alimentación, manual de usuario</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                                        <strong>Usos Recomendados</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseNine" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>Visualización de contenido en 4K, juegos, cine en casa</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+                                        <strong>Fabricante</strong>
+                                    </button>
+                                </h2>
+                                <div id="collapseTen" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <p>Samsung Electronics</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <RelatedProducts />
+        </div>
+    );
+};
