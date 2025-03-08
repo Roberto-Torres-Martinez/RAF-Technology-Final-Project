27463536aa03563cd0e7139df6315ed3c683d0d1
Facundo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-03-08 13:48:29 +0000

    Agregadas las vistas básicas de Smartphones, Tvs y Laptops, asi también su conectividad con la api, trayendo información de las tres y quedando por hacer la imagen de bienvenida de cada una de ellas

--- src/front/js/pages/catalog-phones.js
+++ src/front/js/pages/catalog-phones.js
@@ -1,56 +1,17 @@
 import React from "react";
-import { ProductCardSmall } from "../component/productCardSmall";
+import { WelcomeCatalog } from "../component/welcome-catalog-phones";
+import { CatalogCatalog} from "../component/catalog-catalog-phones";
 
-export const CatalogPhones = () => {
+
+export const Catalog = ({productList}) => {
+
+
     return (
         <>
-            <div className="container">
-                <div className="row col-md-12">
-                    <div className="carrusel-catalogo">
-                        <h1>hola</h1>
-                    </div>
-                </div>
-                <div className="row col-md-12">
-                    <div className="text-center text-white mt-5 mb-5">
-                        <h1>Catálogo de productos móviles</h1>
-                    </div>
-                </div>
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                    <div className="col-md-3 mb-5 d-flex justify-content-center">
-                        <ProductCardSmall />
-                    </div>
-                </div>
-            </div>
+        <div className="p-3">
+        <WelcomeCatalog/>
+        <CatalogCatalog productList={productList}/>
+        </div>
         </>
     )
 }
