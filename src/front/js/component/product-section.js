29ff63f6a950b2f36c23724b01885ea9766a00a0
ALCIDESCONTRERAS <alcidescon@gmail.com>
2025-03-09 10:29:26 +0000

    agregada las imagenes

--- src/front/js/component/product-section.js
+++ src/front/js/component/product-section.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductCard } from "./product-card";
 
-export const ProductSection = ({ productos, products}) => {
- 
+export const ProductSection = ({ Name, products}) => {
+
     return (
         <>
             <div className="container my-5">
-                <h1 className="ms-5 mb-2 text-white">{productos}</h1>
+                <h1 className="ms-5 mb-2 text-white">{Name}</h1>
                 <div className="section container mx-3 pt-5 px-5 pb-1 border-techno border-4 mt-4 mx-auto">
                     <div className="row justify-content-around">
                             {products.map((product, index) =>{
                                 return(
                                     <div className="col-md-3">    
-                                        <ProductCard key={index} model={product.modelo} price={product.precio} description={product.descripcion}/>
+                                        <ProductCard key={index} name={Name} product={product}/>
                                     </div>    
                             )})}; 
                     </div>
