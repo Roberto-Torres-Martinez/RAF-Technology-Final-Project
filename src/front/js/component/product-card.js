94f18de562024bb07e2d100ec56620b784c36690
ALCIDESCONTRERAS <alcidescon@gmail.com>
2025-03-10 09:29:21 +0000

    carta de home con links completos

--- src/front/js/component/product-card.js
+++ src/front/js/component/product-card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 export const ProductCard = ({ product, name }) => {
@@ -11,19 +12,34 @@ export const ProductCard = ({ product, name }) => {
         }
         else {
             images = product.imagen
-        }
-    }
+        };
+    };
+
+
+    let IndividualProduct = "";
+    const validacionProduct = (producto) => {
+        if (producto == "Laptops") {
+            IndividualProduct = "/laptop-info"};
+        if (producto == "Moviles") {
+            IndividualProduct = "/smartphone-info"};
+        if (producto == "TVs") {
+            IndividualProduct = "/tv-info"};
+    };
+
+    
+    
+    
 
     validacionLista(name);
+    validacionProduct(name);
 
     return (
-        <>
-
             <div className="container_card_home">
-
                 <div className="card_home">
                     <div className='container_img'>
-                        <img src={images[0]} />
+                        <Link to={IndividualProduct}>
+                            <img src={images[0]} />
+                        </Link>
                     </div>
                     <span>{product.modelo}</span>
                     <div className='card_description'>
@@ -32,6 +48,5 @@ export const ProductCard = ({ product, name }) => {
                     <h3>{product.precio}</h3>
                 </div>
             </div>
-        </>
     )
 }
--- src/front/js/component/product-section.js
+++ src/front/js/component/product-section.js
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
 import { ProductCard } from "./product-card";
+import { Link } from "react-router-dom";
 
-export const ProductSection = ({ Name, products}) => {
+export const ProductSection = ({ name, products}) => {
+
+    let catalogProduct = "";
+    const validacionProduct = (producto) => {
+        if (producto == "Laptops") {
+            catalogProduct = "/laptops-catalog"};
+        if (producto == "Moviles") {
+            catalogProduct = "/phones-catalog"};
+        if (producto == "TVs") {
+            catalogProduct = "/tvs-catalog"};
+    };
+
+    validacionProduct(name);
+
 
     return (
         <>
             <div className="container my-5">
-                <h1 className="ms-5 mb-2 text-white">{Name}</h1>
+                <h1 className="ms-5 mb-2 text-white">{name}</h1>
                 <div className="section container mx-3 pt-5 px-5 pb-1 border-techno border-4 mt-4 mx-auto">
                     <div className="row justify-content-around">
                             {products.map((product, index) =>{
                                 return(
                                     <div className="col-md-3">    
-                                        <ProductCard key={index} name={Name} product={product}/>
+                                        <ProductCard key={index} name={name} product={product}/>
                                     </div>    
                             )})}; 
                     </div>
                     <div className="w-100">
                         <div className="d-flex justify-content-end">
-                           <button className="product-section-button">Ver más productos</button>
+                            <Link to={catalogProduct}>
+                                <button className="product-section-button">Ver más productos</button>
+                            </Link>
                         </div>
                     </div>
 
