89950138685643f7ecdb33ae652ea95e07f44814
Roberto Torres Martínez <roberxiri@gmail.com>
2025-03-11 18:45:23 +0000

    Estilos y tipografias cambiadas en vistas individuales

--- src/front/js/pages/vistaIndividualTV.js
+++ src/front/js/pages/vistaIndividualTV.js
@@ -51,134 +51,134 @@ export const VistaIndividualTv = () => {
                 </div>
                 <div className="col-md-6 text-white mb-5">
                     <div className="card-body-individual">
-                        <h5 className="card-title sub-titulo mb-3">{tv?.modelo}</h5>
+                        <h5 className="card-title titulo mb-3">{tv?.modelo}</h5>
                         <p className="card-text-score"></p>
-                        <p className="card-text-price sub-titulo">
+                        <p className="card-text-price titulo">
                             <small className="text-white">{totalPrecioEur}</small>
                         </p>
                         <p className="card-text">
-                            <button className="btn-add-cart">
-                                <i className="fa-solid fa-cart-plus"></i> Añadir al carrito
+                            <button className="btn-add-cart texto">
+                                <i className="fa-solid fa-cart-plus mb-1"></i> Añadir al carrito
                             </button>
                         </p>
                         <div className="accordion" id="accordionExample">
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTop" aria-expanded="false" aria-controls="collapseTop">
-                                        <strong>Descripción</strong>
+                                        <strong className="texto">Descripción</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseTop" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.descripcion}</p>
+                                        <p className="texto">{tv?.descripcion}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        <strong>Marca</strong>
+                                        <strong className="texto">Marca</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.marca}</p>
+                                        <p className="texto">{tv?.marca}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                                        <strong>Pantalla</strong>
+                                        <strong className="texto">Pantalla</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.pantalla}</p>
+                                        <p className="texto">{tv?.pantalla}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        <strong>Medidas</strong>
+                                        <strong className="texto">Medidas</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.medidas}</p>
+                                        <p className="texto">{tv?.medidas}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                        <strong>Usos Recomendados</strong>
+                                        <strong className="texto">Usos Recomendados</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.usos_recomendados}</p>
+                                        <p className="texto">{tv?.usos_recomendados}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                                        <strong>Año Modelo</strong>
+                                        <strong className="texto">Año Modelo</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseSix" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.año_modelo}</p>
+                                        <p className="texto">{tv?.año_modelo}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
-                                        <strong>Conectividad</strong>
+                                        <strong className="texto">Conectividad</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseSeven" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.conectividad}</p>
+                                        <p className="texto">{tv?.conectividad}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
-                                        <strong>Contenido De La Caja</strong>
+                                        <strong className="texto">Contenido De La Caja</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseEight" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.contenido_de_la_caja}</p>
+                                        <p className="texto">{tv?.contenido_de_la_caja}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
-                                        <strong>Usos Recomendados</strong>
+                                        <strong className="texto">Usos Recomendados</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseNine" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.usos_recomendados}</p>
+                                        <p className="texto">{tv?.usos_recomendados}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
-                                        <strong>Fabricante</strong>
+                                        <strong className="texto">Fabricante</strong>
                                     </button>
                                 </h2>
                                 <div id="collapseTen" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                                     <div className="accordion-body">
-                                        <p>{tv?.fabricante}</p>
+                                        <p className="texto">{tv?.fabricante}</p>
                                     </div>
                                 </div>
                             </div>
