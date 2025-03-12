94d0a978c7e25ec790a76a0d5d7144cd1613dc8d
ALCIDESCONTRERAS <alcidescon@gmail.com>
2025-03-12 18:49:27 +0000

    zona personal trabajando en las fotos de los usuarios

--- src/front/js/component/personalNavbar.js
+++ src/front/js/component/personalNavbar.js
@@ -1,19 +1,68 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from '../store/appContext';
+
+
 export const PersonalNavbar = () => {
 
     const { store, actions } = useContext(Context)
     const infoUser = store.infoUser
+    const [file, setFile] = useState("");
+
+
+    const changeUploadImage = async (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const sendImage = async () => {
+        const urlBackend = process.env.BACKEND_URL;
+        try {
+            const form = new FormData();
+            form.append("img", file);
+            const response = await fetch(urlBackend + 'upload-image', {
+                method: 'POST',
+                body : form
+            })
+            const data = await response.json();
+            console.log("data", data);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+
+//     const changeUploadImage = async (e) => {
+//         const files = e.target.files[0];
+        
+//         const data = new FormData();
+// -
+//         data.append("file", files);
+//         data.append("upload_preset", "preset_react");
+
+//         const response = await fetch('https://api.cloudinary.com/v1_1/dkowti9mk/image/upload',{
+//             method: 'POST',
+//             body: data
+//         })
+
+//         const file = await response.json()
+//         setUrlImagen( file.secure_url);
+//     }
+
+
 
     return (
         <>
             <div className="d-flex mb-4 pt-4 ms-5">
                 <div className="profile-photo pe-3">
                     <div className="position-relative" style={{ width: "10em", height: "10em" }}>
-                        <div className="profile-circle ratio ratio-1x1">
-                            <img className="photo-personal-zone" src="https://media.cnn.com/api/v1/images/stellar/prod/cnne-212344-monkey-selfie.jpeg?c=16x9&q=h_833,w_1480,c_fill" />
+                        <div>
+                            <input type='file' accept='image/*' onChange={changeUploadImage}/>
+                            <button onClick={sendImage}>subir foto</button>                        
                         </div>
-                        <span className="edit-pen position-absolute  translate-middle rounded-circle p-2 "><i className="p-2 icon-pen fa-solid fa-pen"></i><span className="visually-hidden">unread messages</span></span>
+                        <div className="profile-circle ratio ratio-1x1">
+                            <img className="photo-personal-zone" src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' />
+                        </div>
+                        {/* <span className="edit-pen position-absolute  translate-middle rounded-circle p-2 "><i className="p-2 icon-pen fa-solid fa-pen"></i><span className="visually-hidden">unread messages</span></span> */}
                     </div>
                 </div>
                 <div className="text my-auto fs-3">
