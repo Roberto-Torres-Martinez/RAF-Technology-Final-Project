3e2fd317e8be230b4eae46cbdcb0d5b1fb80be71
Facundo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-03-13 18:43:42 +0000

    Enlace de productos de Carrito, y Tendencia en la home ajustado a cada producto independientemente, y mostrando información del producto que corresponde cada Tendencia en la home

--- src/front/js/component/onfire.js
+++ src/front/js/component/onfire.js
@@ -1,62 +1,66 @@
 import React, { useContext } from "react";
 import {OnFireItem} from "./onfireitem"
 import { Context } from "../store/appContext";
-
+import { Link } from "react-router-dom";
 
 export const Onfire = ({type1, id1, type2, id2}) => {
 
-    // const {store} = useContext(Context)
+    const{store} = useContext(Context)
+    const products1 = store[type1]
+    const products2 = store[type2]
+    let productId1 = ""
+    let productId2 = ""
+    let fireItem1 = ""
+    let fireItem2= ""
 
-    // const products1 = store[type1]
-    // const products2 = store[type2]
-    // let individualProductId1 = ""
-    // let individualProductId2 = ""
-    // let fireItem1 = ""
-    // let fireItem2 = ""
+    const getId1 = () => {
+        if(type1 == "phones"){
+            productId1 = "smartphone_id"
+        } else if(type1 == "tvs"){
+            productId1 = "tv_id"
+        } else if(type1 == "laptops"){
+            productId1 = "laptop_id"
+        }
+    }
 
-    // const getIndividualProductId1 = () => {
-    //     if(type1 == "phones"){
-    //         individualProductId1 = "smartphone_id"
-    //     } else if(type1 == "tvs"){
-    //         individualProductId1 = "tv_id"
-    //     } else if(type1="laptops"){
-    //         individualProductId1 = "laptop_id"
-    //     }
-    // }
+    const getId2 = () => {
+        if(type2 == "phones"){
+            productId2 = "smartphone_id"
+        } else if(type2 == "tvs"){
+            productId2 = "tv_id"
+        } else if(type2 == "laptops"){
+            productId2 = "laptop_id"
+        }
+    }
 
-    // const getIndividualProductId2 = () => {
-    //     if(type2 == "phones"){
-    //         individualProductId2 = "smartphone_id"
-    //     } else if(type2 == "tvs"){
-    //         individualProductId2 = "tv_id"
-    //     } else if(type2="laptops"){
-    //         individualProductId2 = "laptop_id"
-    //     }
-    // }
-
-    // getIndividualProductId1()
-    // getIndividualProductId2()
-
-    // const findFireItems = (firstProducts, firstId, secondProducts, secondId) => {
-
-    //     fireItem1 = firstProducts.filter((individualProduct)=> individualProduct[individualProductId1] == firstId)
-    //     fireItem2 = secondProducts.filter((individualProduct)=> individualProduct[individualProductId2] == secondId)
-
-    // }
-
-    // findFireItems(products1, id1, products2, id2)
-
-    // console.log(fireItem1[0])
-    // console.log(fireItem2[0])
+    getId1()
+    getId2()
     
+    products1.forEach((element)=>{
+        if(element[productId1] == id1){
+            fireItem1 = element
+        }
+        })
+
+    products2.forEach((element)=>{
+        if(element[productId2] == id2){
+            fireItem2 = element
+        }
+        })
+
+
 
     return(<>
     <div id="inicio" className="row d-flex justify-content-around py-3 px-0 mx-auto ">
         <div className="col-md-6">
-    <OnFireItem/>
+            <Link to={`/smartphone-info/${id1}`}>
+    <OnFireItem item={fireItem1} itemName={type1}/>
+    </Link>
     </div>
     <div className="col-md-6">
-    <OnFireItem/>
+    <Link to={`/tv-info/${id2}`}>
+    <OnFireItem item={fireItem2} itemName={type2}/>
+    </Link>
     </div>
     </div>
     </>)
--- src/front/js/component/onfireitem.js
+++ src/front/js/component/onfireitem.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export const OnFireItem = () => {
+export const OnFireItem = ({item, itemName}) => {
+
+
+
+  // const validacionLista = (producto) => {
+  //     if (producto == "laptops" || producto == "phones") {
+  //         const color = (producto.colores[0].toLowerCase()).replace(/ /g, "_");
+  //         images = item.imagen[color];
+        
+  //     }
+  //     else {
+  //         images = item.imagen
+         
+  //     }
+  // };
+
+  // validacionLista(itemName)
+
+  let image = ""
+
+  const validacionLista = () =>{
+    if(itemName == "phones" || itemName == "laptops"){
+      const color = (item.colores?.[0].toLowerCase())?.replace(/ /g,"_");
+      image = item.imagen?.[color]
+    }
+    else {
+      image = item.imagen
+    }
+  }
+
+  
+  validacionLista()
+  
+  console.log(image)
+
+
     return(<>
     <div className="card position-relative text-bg-dark">
 
         <div className="ratio ratio-16x9">
-         <img src="https://www.mundodeportivo.com/alfabeta/hero/2024/09/devil-may-cry.webp?width=768&aspect_ratio=16:9&format=nowebp" className="card-img" alt="..."/>
+         <img src={image?.[0]} className="card-img" alt="..."/>
         </div>
     <span className="ribbon position-absolute top-0 start-0 px-2 rounded text-bg-danger">TENDENCIA<span className="visually-hidden">unread messages</span></span>
   <div className="card-img-overlayp-0 fire-container text-end p-3">
-    <h5 className="card-title">Devil May Cry</h5>
-    <p className="card-text"> Una historia que te dejará los pelos de punta</p>
+    <h5 className="card-title">{item.modelo}</h5>
+    <p className="card-text"> {item.descripcion}</p>
   </div>
 </div>
     </>)
