d92f1dcecb26bc9bb960d2a9d708e03bd05237df
ALCIDESCONTRERAS <alcidescon@gmail.com>
2025-03-15 18:18:50 +0000

    cambiado la pasarela de pago

--- src/front/js/component/formPayment.js
+++ src/front/js/component/formPayment.js
@@ -1,70 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { PaymentElement } from "@stripe/react-stripe-js";
 
-const urlBackend = process.env.BACKEND_URL;
+
 
 export const FormPayment = () => {
     const stripe = useStripe();
     const elements = useElements();
-    const [clientSecret, setClientSecret] = useState('');
     const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState(null);
 
-    const paymentIntent = async () => {
-        const response = await fetch(urlBackend + 'payment', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                'amount': 1000,
-                'currency': 'eur'
-            })
-        })
-        const data = await response.json();
-        if (data.client_secret) {
-            setClientSecret(data.client_secret);
-        } else {
-            console.error('No se recibió el client_secret');
-        }
-    };
 
-    useEffect(() => {
-        paymentIntent();
-    }, []);
+   
+      
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if (!stripe || !elements) {
-            return;
-        }
+        if (!stripe || !elements) return;
 
         setLoading(true);
 
-        const { error, paymentIntent } = await stripe.confirmCardPayment(
-            clientSecret, {
-            payment_method: {
-                card: elements.getElement(CardElement),
+        const { error, paymentIntent } = await stripe.confirmPayment({
+            elements, 
+            confirmParams:{
+                return_url: 'https://super-duper-space-adventure-pjpjqx6q46qp27jgx-3000.app.github.dev/cart'
                 },
+                redirect: "if_required"
             },
         );
 
-        setLoading(false);
+         setLoading(false);
 
         if (error) {
-            console.log('[error]', error);
+            setMessage(error.message)
         } else if (paymentIntent.status === 'succeeded') {
-            console.log('Payment succeeded!');
+            setMessage("Pago confirmado!!!");
         }
         else {
-            console.log('some error')
+            setMessage("Estado Inesperado")
         }
     };
 
     return (
         <>
             <form className="w-50 bg-light mx-auto" onSubmit={handleSubmit}>
-                <CardElement />
+                <PaymentElement/>
                 <button type="submit" disabled={!stripe || loading}>
-                    Pay
+                    <span>
+                        {loading ? 'Procesando' : 'Pagar'}
+                    </span>
                 </button>
             </form>
 
--- src/front/js/pages/PasarelaPago.js
+++ src/front/js/pages/PasarelaPago.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FormPayment } from "../component/formPayment";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
+const urlBackend = process.env.BACKEND_URL;
 const stripePromise = loadStripe('pk_test_51QxWTcF1M5ixil84DV7yx8UcwpGMJXggd0XSjMwT493HuieKbKIf3nWo94YaWDYrl4A781CqNpXw5vww4Q3p3IBv00oAd5cnVd')
 export const PasarelaPago = () => {
+    const [clientSecret, setClientSecret] = useState(null);
+
+    const paymentIntent = async () => {
+        const response = await fetch(urlBackend + 'payment', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                'amount': 1000,
+                'currency': 'eur'
+            })
+        })
+        const data = await response.json();
+        if (data.client_secret) {
+            setClientSecret(data.client_secret);
+        } else {
+            console.error('No se recibió el client_secret');
+        }
+    };
+
+    console.log(clientSecret);
+
+    useEffect(() => {
+        paymentIntent();
+    }, []);
 
     return(
-        <Elements stripe={stripePromise}>
-            <FormPayment/>
-        </Elements>
+        <>
+            {stripePromise && clientSecret ? (
+                <Elements stripe={stripePromise} options={{ clientSecret }}>
+                    <FormPayment />
+                </Elements>
+            ) : (
+                <p>Cargando pago...</p>
+            )}
+        </>
     );
 }
