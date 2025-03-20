0f140011cdd5f5f2b5bf5a1c38ec40267677441c
Roberto Torres Martínez <roberxiri@gmail.com>
2025-03-20 12:44:35 +0000

    Retoques parte visual imagenes carrusel

--- src/front/js/component/productCardSmall.js
+++ src/front/js/component/productCardSmall.js
@@ -30,8 +30,8 @@ export const ProductCardSmall = ({ product }) => {
     return (
         <div className="container content-small-card">
             <div className="col-md-12">
-                <div className="card border-0 w-100 mb-4 text-black rounded-4 mx-auto d-flex flex-column" onClick={sendToIndividualProduct} style={{ height: '400px', backgroundColor: "rgb(255, 255, 255)" }}>
-                    <img src={imagen} className="card-img-top" style={{}} />
+                <div className="card border-0 w-100 mb-4 text-black rounded-4 mx-auto d-flex flex-column" onClick={sendToIndividualProduct} style={{ height: '420px', backgroundColor: "rgb(255, 255, 255)" }}>
+                    <img src={imagen} className="card-img-top" style={{height: '150px'}} />
                     <div className="card-body border-card border-5 product d-flex flex-column justify-content-between">
                         <h5 className="card-title mb-0"><a href="#">{product.modelo}</a></h5>
                         <p className="fs-4 my-0 py-0"><b>{product.precio}</b></p>
